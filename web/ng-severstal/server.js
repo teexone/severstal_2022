7eb189c0a21102d009ced6fc0b59521bd11e7e9e
Aleksandr Lobanov <alex.teexone@gmail.com>
2022-03-10 14:14:27 +0300

    Deployment server

--- web/ng-severstal/server.js
+++ web/ng-severstal/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 app = express()
-app.use('/', express.static('dist/ng-severstal'))
+app.use(express.static(path.join(__dirname, 'dist/ng-severstal')))
+
+app.get('/*', async (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist/ng-severstal', 'index.html'));
+});
 
 app.listen(80, () => {
   console.log("Server is running")
 })
+
